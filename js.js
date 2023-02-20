3368112ea4f6201d846cfe3b9a7c0e329aa7d60b
Makson <nooplace55@gmail.com>
2023-02-20 22:31:02 +0600

    021 Do while Loops

--- js.js
+++ js.js
@@ -1,18 +1,36 @@
-// 020 While Loops
+// // 020 While Loops
 
- let i =0;
+//  let i =0;
 
- while(i < 5){
-     console.log(i);
-     i++;
- }
+//  while(i < 5){
+//      console.log(i);
+//      i++;
+//  }
 
-//  While with Array
+// //  While with Array
 
-const names = ['Max','Rax','Tax'];
-let i = 0;
+// const names = ['Max','Rax','Tax'];
+// let i = 0;
 
-while(i < names.length){
-    console.log(names[i]);
+// while(i < names.length){
+//     console.log(names[i]);
+//     i++;
+// }
+
+
+// 021 Somre more aboout While Loops
+
+// do while loops
+
+let i = 5;
+
+do{
+    console.log(i);
     i++;
-}
+} while( i < 5);
+console.log(i)
+
+// Задаем значение переменной i = 4
+// делаем do ни смотря ни на что
+// затем идет проверка условия в while
+// Если условие false - цикл прерывается. Но значение в DO - будет произведено
