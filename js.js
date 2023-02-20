08f732f3f08a58350678231a3c804e6bba74c516
Makson <nooplace55@gmail.com>
2023-02-20 21:32:02 +0600

    017 Type Conversion

--- js.js
+++ js.js
@@ -1,5 +1,33 @@
-let age = 25;
-console.log(age === 25);
-console.log(age === '25');
-console.log(age !== 25);
-console.log(age !== '25');
+// let age = 25;
+// console.log(age === 25);
+// console.log(age === '25');
+// console.log(age !== 25);
+// console.log(age !== '25');
+
+
+// 017 Type Conversion 
+
+//Превращение стринга в намбер
+
+let score = '100';
+
+score = Number(score);
+console.log(score + 1)
+
+//Превращение инта в намбер + проверка типа
+
+let result = String(100);
+
+console.log(result, typeof result);
+
+// Booleans
+
+let val = Boolean(0); // нулевой булиан равен false
+let val2 = Boolean(1); // if let ≠ 0 -> будет true
+
+let val3 = Boolean(''); // Если стринговый булин не пустой - true, пуст false
+
+
+console.log(val, val2, val3)
+
+////////////////////////////////
