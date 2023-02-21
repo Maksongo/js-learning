7583faadf6cb5af0eabb2b1262cd0f1ab59e611a
Makson <nooplace55@gmail.com>
2023-02-21 19:58:29 +0600

    022 If statements lession

--- js.js
+++ js.js
@@ -1,36 +1,7 @@
-// // 020 While Loops
+// 022 If Statements
 
-//  let i =0;
+const password  = 'pass';
 
-//  while(i < 5){
-//      console.log(i);
-//      i++;
-//  }
-
-// //  While with Array
-
-// const names = ['Max','Rax','Tax'];
-// let i = 0;
-
-// while(i < names.length){
-//     console.log(names[i]);
-//     i++;
-// }
-
-
-// 021 Somre more aboout While Loops
-
-// do while loops
-
-let i = 5;
-
-do{
-    console.log(i);
-    i++;
-} while( i < 5);
-console.log(i)
-
-// Задаем значение переменной i = 4
-// делаем do ни смотря ни на что
-// затем идет проверка условия в while
-// Если условие false - цикл прерывается. Но значение в DO - будет произведено
+if(password.length >= 8){
+    console.log('nice password,bruh');
+}
