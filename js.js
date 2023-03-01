1ab5c995ea274f66dff23a666224a2c46045ae25
Makson <nooplace55@gmail.com>
2023-03-01 22:36:29 +0600

    027 Switch Statements

--- js.js
+++ js.js
@@ -1,17 +1,27 @@
-// 026 break and continue
+// 027 Switch Statements
 
-const scores = [50, 25, 0, 30, 100, 20 ,10];
+const grade = 'A';
 
-for(let i = 0; i < scores.length; i++){
-
-    if(scores[i] === 0){
-        continue;
-    }
-
-    console.log('your score: ', scores[i]);
-
-    if(scores[i] === 100){
-        console.log('congrats, you got the top score!')
+switch(grade){
+    case 'A':
+        console.log('you got an A!');
         break;
-    }
+    case 'B':
+        console.log('you got an B!');
+        break;
+    case 'C':
+        console.log('you got an C!');
+        break;
+    case 'D':
+         console.log('you got an D!');
+         break;
+    case 'E':
+         console.log('you got an E!');
+        break;
+    default:
+        console.log('not a vaild grade');                  
 }
+
+
+/// it's a strict comparison 
+/// case A = будет сравниваться строго, включая тип переменных
