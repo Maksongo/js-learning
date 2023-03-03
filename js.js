e26d29978af6ad4128de1e8b8d7fc85a8a0650c6
Makson <nooplace55@gmail.com>
2023-03-03 21:57:17 +0600

    031 Arguments & Parameters

--- js.js
+++ js.js
@@ -1,25 +1,9 @@
-// 030 Function Declarations & Expressions
+// 031 Arguments & Parameters
 
 
-// function declaration
-
-function greet(){
-    console.log('hello there');
-}
-
-// function expression
-
-const speak = function(){
-    console.log('good day!');
+const speak = function(name = 'luigi', time = 'night'){
+    console.log(`good ${time} ${name}`);
 };
 
-// greet();
-// greet();
-// greet();
-
 speak();
-speak();
-speak();
-
-// Function declaration – может быть в любом порядке ( hoisting ) функцию вызвать можно в любом месте
-// Function expression – функция должна быть объявлена исключительно до того, как мы ее будет вызывать
+speak('shaun');
