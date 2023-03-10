b8ab6180d06bc15594c55dc9609234063d500aee
Makson <nooplace55@gmail.com>
2023-03-10 17:43:35 +0600

    032 Returning Values

--- js.js
+++ js.js
@@ -1,9 +1,14 @@
-// 031 Arguments & Parameters
+// 032 Returning Values
 
+// const speak = function(name = 'luigi', time = 'night'){
+//     console.log(`good ${time} ${name}`)
+// };
 
-const speak = function(name = 'luigi', time = 'night'){
-    console.log(`good ${time} ${name}`);
-};
+const CalcArea = function(radius){
+    return 3.14 * radius**2;
+    // let area = 3.14 * radius**2;
+    // return area; - ALSO WORKS
+}
 
-speak();
-speak('shaun');
+const area = CalcArea(5);
+console.log(area);
