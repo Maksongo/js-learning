9c51611d680811a498a569c3dfdb3ce80953fcd3
Makson <nooplace55@gmail.com>
2023-03-13 22:48:44 +0600

    034 Functions vs Methods

--- js.js
+++ js.js
@@ -1,43 +1,15 @@
-// 033 Arrow Functions
+// 034 Functions vs Methods
 
-//regular function
+const name = 'shaun';
 
-// const CalcArea = function(radius){
-//     return 3.14 * radius**2;
-// }
+// functions
 
+const greet = () => 'hello';
 
-// arrow function
+let resultOne = greet();
+console.log(resultOne);
 
-// const CalcArea = (radius) => {
-//     return 3.14 * radius**2;
-// }
+// methods
 
-
-// Сокращения ниже:
-
-// const CalcArea = (radius) => 
-// скобки у (radius) можно убрать если это единственная переменная. Если несколько или 0 – то () нужны
-
-// const CalcArea = radius =>  3.14 * radius**2;
-// если return всего 1 – можно использовать следующее правописание. Без использования {} и return word
-
-// Some exercises:
-
-// const bill = function(products, tax){
-//     let total = 0;
-//     for(let i = 0; i < products.length; i++){
-//         total += products[i] + products[i] * tax;
-//     }
-//     return total;
-// }
-
-const bill = (products, tax) => {
-    let total = 0;
-    for(let i = 0; i < products.length; i++){
-        total += products[i] + products[i] * tax;
-    }
-    return total;
-}
-
-console.log(bill([10,15,30], 0.2));
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
