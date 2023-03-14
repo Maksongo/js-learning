163656666fea3cfd3c015bdd1f01827f43c52444
Makson <nooplace55@gmail.com>
2023-03-14 21:25:49 +0600

    039 Adding Methods

--- js.js
+++ js.js
@@ -1,21 +1,24 @@
-// 037 Creating an Object Literal
+// 039 Adding Methods
 
 let user = {
     name: 'crystal',
     age: 30,
     email: 'trap@trap.ru',
     location: 'omsk',
-    drinks: ['beer', 'vodka']
+    drinks: ['beer', 'vodka'],
+    login: function(){
+        console.log('the user logged in');
+    },
+    logout: function(){
+        console.log('the user logged out');
+    },
+    logDrinks: function (){
+
+    }
 };
 
-console.log(user);
-console.log(user.name);
+user.login();
+user.logout();
 
-// меняем значение в объекте
-user.age = 35;
-console.log(user.age);
-
-// так-же можно так запрос делать
-console.log(user['name']);
-user['name'] = 'chun-li';
-console.log(user['name']);
+const name = 'mario';
+name.toUpperCase();
