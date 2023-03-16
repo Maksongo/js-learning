944af9d190151f522e885e9c56b0cdde711f3f07
Makson <nooplace55@gmail.com>
2023-03-16 16:21:39 +0600

    button on click test

--- js.js
+++ js.js
@@ -1,24 +1,24 @@
-// 039 Adding Methods
+const peopleInFiller = () => {
+    const ul = document.querySelector('.people');
+    
+    const people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+    
+    let html = ``;
+    
+    people.forEach(person => {
+        // create html template
+        html += `<li style="color: purple">${person}</li>`;
+    });
+    
+    console.log(html);
+    ul.innerHTML = html;
+}
 
-let user = {
-    name: 'crystal',
-    age: 30,
-    email: 'trap@trap.ru',
-    location: 'omsk',
-    drinks: ['beer', 'vodka'],
-    login: function(){
-        console.log('the user logged in');
-    },
-    logout: function(){
-        console.log('the user logged out');
-    },
-    logDrinks: function (){
 
-    }
+let buttons = document.getElementsByTagName('button');
+
+for (let i = 0, len = buttons.length; i < len; ++i) {
+    const singleButton = buttons[i];
+    singleButton.addEventListener("click", peopleInFiller
+    )
 };
-
-user.login();
-user.logout();
-
-const name = 'mario';
-name.toUpperCase();
