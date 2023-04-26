c786230ac96d18be24517298a5e64af9962ae764
Makson <nooplace55@gmail.com>
2023-04-26 18:43:14 +0600

    91 урок - callback functions GET

---
+++ 046+/091/js.js
@@ -0,0 +1,23 @@
+const getTodos = (callback) => {
+  const request = new XMLHttpRequest();
+
+  request.addEventListener("readystatechange", () => {
+    if (request.readyState === 4 && request.status === 200) {
+      callback(undefined, request.responseText);
+    } else if (request.readyState === 4) {
+      callback("coud not fetch data", undefined);
+    }
+  });
+  git commit --amend --no-edit --date=now
+  request.open("GET", "https://jsonplaceholder.typicode.com/todos");
+  request.send();
+};
+
+getTodos((err, data) => {
+  console.log("callback fired");
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
