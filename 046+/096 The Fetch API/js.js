d31874e8e5a2ca504a68903e4d5e651197d83817
Makson <nooplace55@gmail.com>
2023-05-03 22:19:45 +0600

    96 урок - The Fetch API ( теория + код )

--- 046+/096 The Fetch API/js.js
+++ 046+/096 The Fetch API/js.js
@@ -1,1 +1,10 @@
 // Fetch API
+
+fetch('todos/luigi.json').then((response) => {
+  console.log('resolved', response);
+  return response.json();
+}).then(data => {
+  console.log(data);
+}).catch((err) => {
+  console.log('rejected', err)
+});
