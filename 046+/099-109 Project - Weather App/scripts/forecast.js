ed3a5ee8b68829987844567b9dd56ed7d0d5b803
Makson <nooplace55@gmail.com>
2023-05-11 20:02:09 +0600

    Get Weather + Get City Functions работающие вместе

--- 046+/099-109 Project - Weather App/scripts/forecast.js
+++ 046+/099-109 Project - Weather App/scripts/forecast.js
@@ -1,33 +1,36 @@
 // Weather API
 
-const key = 'Miz1PBG3gbDUqWkG8RMo4UNr1Og2EIAO';
+const key = "Miz1PBG3gbDUqWkG8RMo4UNr1Og2EIAO";
 
 // Get Weather Information
 
 const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
 
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-    const query = `${id}?apikey=${key}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
 
-    const response = await fetch(base + query);
-    const data = await response.json();
-
-}
+  return data[0];
+};
 
 // Get City information
 
 const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
 
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-    const query = `?apikey=${key}&q=${city}`;
-    
-    const response = await fetch(base + query);
-    const data = await response.json();
+  const response = await fetch(base + query);
+  const data = await response.json();
 
-    return data[0];
-
+  return data[0];
 };
 
-getCity('manchester')
-    .then(data => console.log(data))
-    .catcg(err => console.log(err));
+getCity("manchester")
+  .then((data) => {
+    return getWeather(data.Key);
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.log(err));
